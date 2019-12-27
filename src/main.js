47abde949ea48a779ca1e227e12cb84d66cc2e47
Yeonwoo Kim <wiskingdom@gmail.com>
2019-12-27 17:13:57 +0900

    Add firebase

--- src/main.js
+++ src/main.js
@@ -3,11 +3,21 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./quasar";
+import fireapp from './fireapp';
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+let app = '';
+
+Vue.prototype.$auth = fireapp.auth();
+Vue.prototype.$db = fireapp.database();
+
+fireapp.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App),
+    }).$mount('#app');
+  }
+});
