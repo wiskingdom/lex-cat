7293a43783ad8a2c4a1d9bb181719e0d2872cc2f
Yeonwoo Kim <wiskingdom@gmail.com>
2020-01-20 21:44:31 +0900

    Deploy v0.1.3

--- src/fireapp.js
+++ src/fireapp.js
@@ -1,7 +1,7 @@
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
-//import firebaseConfig from './.firebase.config.json';
-import firebaseConfig from './.firebase.test.config.json';
+import firebaseConfig from './.firebase.config.json';
+//import firebaseConfig from './.firebase.test.config.json';
 
 export default firebase.initializeApp(firebaseConfig);
