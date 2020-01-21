c63b806b99063520411692148d995cf92e75e507
Yeonwoo Kim <wiskingdom@gmail.com>
2020-01-21 19:41:16 +0900

    Update guide

--- src/views/MainAbout.vue
+++ src/views/MainAbout.vue
@@ -3,9 +3,9 @@
     <p></p>
     <p>
       <a
-        href="https://firebasestorage.googleapis.com/v0/b/lex-cat.appspot.com/o/guide%2FNC-%EC%A0%84%EC%9E%90%EC%82%AC%EC%A0%84%EA%B5%AC%EC%B6%95_%EA%B0%80%EC%9D%B4%EB%93%9C%EB%9D%BC%EC%9D%B8_%EC%88%98%EC%A0%95%EC%A4%91_0109.docx?alt=media&token=df90a1b5-7686-4f83-aba2-5072d93fd055"
+        href="https://firebasestorage.googleapis.com/v0/b/lex-cat.appspot.com/o/guide%2FNC-%EC%A0%84%EC%9E%90%EC%82%AC%EC%A0%84%EA%B5%AC%EC%B6%95_%EA%B0%80%EC%9D%B4%EB%93%9C%EB%9D%BC%EC%9D%B8_%EC%88%98%EC%A0%95%EC%A4%91_0121.docx?alt=media&token=0412016b-9eed-49d6-8d12-867d2b4ea1ab"
         download
-        >가이드라인 다운로드</a
+        >NC-전자사전구축_가이드라인_20200121 다운로드</a
       >
     </p>
   </div>
