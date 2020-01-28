c2ae03a4b106ba9b4263c7101036f934359bfa25
Yeonwoo Kim <wiskingdom@gmail.com>
2020-01-28 17:42:08 +0900

    Update guideline link

--- src/views/MainAbout.vue
+++ src/views/MainAbout.vue
@@ -3,9 +3,9 @@
     <p></p>
     <p>
       <a
-        href="https://firebasestorage.googleapis.com/v0/b/lex-cat.appspot.com/o/guide%2FNC-%EC%A0%84%EC%9E%90%EC%82%AC%EC%A0%84%EA%B5%AC%EC%B6%95_%EA%B0%80%EC%9D%B4%EB%93%9C%EB%9D%BC%EC%9D%B8_%EC%88%98%EC%A0%95%EC%A4%91_0121.docx?alt=media&token=0412016b-9eed-49d6-8d12-867d2b4ea1ab"
+        href="https://firebasestorage.googleapis.com/v0/b/lex-cat.appspot.com/o/guide%2Feconomy%2FNC-%EC%A0%84%EC%9E%90%EC%82%AC%EC%A0%84%EA%B5%AC%EC%B6%95_%EA%B0%80%EC%9D%B4%EB%93%9C%EB%9D%BC%EC%9D%B8_%EC%88%98%EC%A0%95%EC%A4%91_0128.docx?alt=media&token=a55b67fc-2bfa-4a8d-bf1a-3997d30af25c"
         download
-        >NC-전자사전구축_가이드라인_20200121 다운로드</a
+        >NC-전자사전구축_가이드라인_20200128 다운로드</a
       >
     </p>
   </div>
