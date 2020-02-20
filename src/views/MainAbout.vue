30f2961ee1a0ad51c36abbe0c0b4debd79b2ea23
Yeonwoo Kim <wiskingdom@gmail.com>
2020-02-20 21:00:13 +0900

    Update guidline

--- src/views/MainAbout.vue
+++ src/views/MainAbout.vue
@@ -3,9 +3,9 @@
     <p></p>
     <p>
       <a
-        href="https://firebasestorage.googleapis.com/v0/b/lex-cat.appspot.com/o/guide%2Feconomy%2FNC-%EC%A0%84%EC%9E%90%EC%82%AC%EC%A0%84%EA%B5%AC%EC%B6%95_%EA%B0%80%EC%9D%B4%EB%93%9C%EB%9D%BC%EC%9D%B8_%EC%88%98%EC%A0%95%EC%A4%91_0128.docx?alt=media&token=a55b67fc-2bfa-4a8d-bf1a-3997d30af25c"
+        href="https://firebasestorage.googleapis.com/v0/b/lex-cat.appspot.com/o/guide%2Feconomy%2FNC-%EC%A0%84%EC%9E%90%EC%82%AC%EC%A0%84%EA%B5%AC%EC%B6%95_%EA%B0%80%EC%9D%B4%EB%93%9C%EB%9D%BC%EC%9D%B8_%EA%B2%BD%EC%A0%9C%EB%8F%84%EB%A9%94%EC%9D%B8_%EC%88%98%EC%A0%95%EC%A4%91_0218.docx?alt=media&token=eec67e0f-75f9-446c-a9e7-e8dc46c07710"
         download
-        >NC-전자사전구축_가이드라인_20200128 다운로드</a
+        >NC-전자사전구축_경제도메인_가이드라인_20200218 다운로드</a
       >
     </p>
   </div>
